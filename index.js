0c066b17ca08ad517cb41028aed08cb70999ff5a
Mateusz <kontakt@mateuszoracz.pl>
2016-11-22 14:52:18 +0100

    added delete person, get peoples, add relationship between people, add people

--- index.js
+++ index.js
@@ -9,8 +9,34 @@ const db = new neo4j('http://neo4j:password@neo4j:7474');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// ADD PERSON
-//
+// [GET] ALL PEOPLE
+// http://localhost:3000/people
+app.get('/people', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  db.cypherQuery('MATCH (n:Person) RETURN n', {}, function(err, results) {
+    res.send(results.data);
+    err != true ?
+    res.status(200).send() :
+    res.status(404).send();
+  });
+});
+
+// [GET] SINGLE PERSON BY NAME
+// http://localhost:3000/person/John
+app.get('/person/:name', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  db.cypherQuery('MATCH (p:Person) WHERE p.name = "' + req.params.name + '" RETURN p',
+   {}, function(err, results) {
+     res.send(results.data);
+     err != true ?
+     res.status(200).send() :
+     res.status(404).send();
+   });
+});
+
+// [POST] ADD PERSON
+// // http://localhost:3000/person
+
 // {
 //  "name": "John",
 //  "sex": "male"
@@ -22,9 +48,43 @@ app.post('/person', function(req, res) {
     sex: req.body.sex
   }, ['Person'], function(err) {
     err != true ?
-    res.status(201).send() :
+    res.status(200).send() :
     res.status(404).send();
   });
+});
+
+// [POST] ADD RELATIONSHIP BEETWEN PEOPLE
+// // http://localhost:3000/knows
+
+// {
+//  "name1": "John",
+//  "name2": "Ann"
+// }
+app.post('/know', function(req, res) {
+  req.accepts('application/json');
+  db.cypherQuery('MATCH (a:Person { name: "' + req.body.name1 + '" }), (b:Person { name: "' + req.body.name2 + '" }) CREATE (a)-[:KNOWS]->(b)',
+    {}, function(err) {
+      err != true ?
+      res.status(201).send() :
+      res.status(404).send();
+    });
+});
+
+
+// [DELETE] DELETE PERSON
+// // http://localhost:3000/person
+
+// {
+//  "name": "John"
+// }
+app.delete('/person', function(req, res) {
+  req.accepts('application/json');
+  db.cypherQuery('MATCH (p:Person)-[rel:KNOWS]->() WHERE p.name = "' + req.body.name + '" DELETE rel',
+    {}, function(err) {
+      err != true ?
+      res.status(204).send() :
+      res.status(409).send();
+    });
 });
 
 app.listen(3000, function() {
