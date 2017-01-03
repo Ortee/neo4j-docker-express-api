6d1c6c2496b1bb05053f71da5203e824e3605ed7
Mateusz <kontakt@mateuszoracz.pl>
2017-01-03 23:41:32 +0100

    added more jasmine tests

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -19,4 +19,100 @@ describe("Neo4j simple API by Ortee", function() {
     });
   });
 
+  describe('description', () => {
+    it("POST /api/person -  ADD MALE", function(done){
+      request({
+        url: url + '/api/person',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({name: 'Adam123', sex: 'male'})
+      }, function(error, response, body){
+          if(!error) {
+            expect(response.statusCode).toBe(201);
+            done();
+          }
+      });
+    });
+
+    it("POST /api/person -  ADD FEMALE", function(done){
+      request({
+        url: url + '/api/person',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({name: 'Ann123', sex: 'female'})
+      }, function(error, response, body){
+          if(!error) {
+            expect(response.statusCode).toBe(201);
+            done();
+          }
+      });
+    });
+
+    it("GET /api/person/* GET MALE", function(done) {
+      request.get(url + '/api/person/Adam123', function(error, response, body) {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+
+    it("GET /api/person/* GET FEMALE", function(done) {
+      request.get(url + '/api/person/Ann123', function(error, response, body) {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+
+    it("POST /api/know ADD RELATIONSHIP", function(done){
+      request({
+        url: url + '/api/know',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({name1: 'Ann123', name2: 'Adam123'})
+      }, function(error, response, body){
+          if(!error) {
+            expect(response.statusCode).toBe(201);
+            done();
+          }
+      });
+    });
+
+    it("DELETE /api/person DELETE MALE", function(done){
+      request({
+        url: url + '/api/person',
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({name: 'Adam123'})
+      }, function(error, response, body){
+          if(!error) {
+            expect(response.statusCode).toBe(204);
+            done();
+          }
+      });
+    });
+
+    it("DELETE /api/person DELETE FEMALE", function(done){
+      request({
+        url: url + '/api/person',
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({name: 'Ann123'})
+      }, function(error, response, body){
+          if(!error) {
+            expect(response.statusCode).toBe(204);
+            done();
+          }
+      });
+    });
+    
+  });
 });
